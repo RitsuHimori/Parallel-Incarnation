7604b620c2b97b651ed5d77e57f46534d29984f7
RitsuHimori <140252509+RitsuHimori@users.noreply.github.com>
2026-01-07 14:22:00 +0800

    Add files via upload

---
+++ 3/test.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+  //AMAN: Semua elemen HTML dijamin sudah ada.
+  const header = this.document.querySelector('h1');
+  header.style.color = 'blue';
+  console.log('Halaman dan semya asetnya sudah termuat.');
+});
